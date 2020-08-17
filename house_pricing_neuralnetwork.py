5fc13cc778ef3597e61cb99197a3448d95e72048
Gayathri K <60387180+Gayathri-K-2000@users.noreply.github.com>
2020-08-17 20:23:04 +0530

    Update house_pricing_neuralnetwork.py

--- house_pricing_neuralnetwork.py
+++ house_pricing_neuralnetwork.py
@@ -6,8 +6,9 @@ model= keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 x=np.array([-1.0,0.0,1.0,2.0,3.0,4.0], dtype= float)
-y=np.array([-3.0,-1.0,1.0,3.0,5.0,7.0], dtype= float)
+y=np.array([-3.0,-1.0,1.0,3.0,5.0,7.0], dtype= float) 
 
 model.fit(x,y, epochs=500)
 
 print(model.predict([8.0]))
+# OUTPUTS THE Y VALUE IDENTIFYING THE PATTERN Y= 2X-1
